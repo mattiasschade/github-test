40eddafad0928882ef16918ae85676762dc53174
Mattias Schade <mattiasschade@gmail.com>
2023-03-21 12:50:03 -0500

    added while loop

--- git-test.rb
+++ git-test.rb
@@ -1,16 +1,20 @@
 puts "Hello! I'm thinking of a number between 1 and 100. Please enter a number: "
 number = gets.chomp.to_i
 
-if number < 47
-  puts "too low, try again: "
-  number = gets.chomp.to_i
-elsif number == 47
-  puts "You win!"
-elsif number > 47
-  puts "too high, try again: "
-  number = gets.chomp.to_i
+
+while number != 47
+  if number < 47
+    puts "too low, try again: "
+    number = gets.chomp.to_i
+  elsif number == 47
+    break
+  elsif number > 47
+    puts "too high, try again: "
+    number = gets.chomp.to_i
+  end
 end
 
+puts "You win!"
 
 
 
