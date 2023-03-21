1378f2f6c1fd96e6883bc0370c0f861f13ec20b4
Mattias Schade <mattiasschade@gmail.com>
2023-03-21 12:47:08 -0500

    added conditional statements

--- git-test.rb
+++ git-test.rb
@@ -1,4 +1,19 @@
 puts "Hello! I'm thinking of a number between 1 and 100. Please enter a number: "
 number = gets.chomp.to_i
 
+if number < 47
+  puts "too low, try again: "
+  number = gets.chomp.to_i
+elsif number == 47
+  puts "You win!"
+elsif number > 47
+  puts "too high, try again: "
+  number = gets.chomp.to_i
+end
+
+
+
+
+
+
 p number
