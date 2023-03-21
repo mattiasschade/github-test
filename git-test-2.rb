cac097ed1107465de46b2272206e8282b0ca00c9
Mattias Schade <mattiasschade@gmail.com>
2023-03-21 12:58:16 -0500

    created motorcycle class

---
+++ git-test-2.rb
@@ -0,0 +1,10 @@
+class Motorcycle
+  def initialize(make, model, color, year)
+    @make = make
+    @model = model
+    @color = color
+    @year = year
+  end
+end
+
+motorcycle = Motorcycle.new("triumph", "street twin", "black", "2017")
